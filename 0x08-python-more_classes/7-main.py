c6565fedd1875fd053c3c9f40cf9be8c024e9628
BiGStEvE1 <haimoudmustapha@gmail.com>
2023-09-06 02:29:07 -0700

    0x08-python-more_classes

--- 0x08-python-more_classes/7-main.py
+++ 0x08-python-more_classes/7-main.py
@@ -1,26 +1,106 @@
+
 #!/usr/bin/python3
-Rectangle = __import__('7-rectangle').Rectangle
+"""Mdule 7-rectangle
+Defines a Rectangle class.
+"""
 
-my_rectangle_1 = Rectangle(8, 4)
-print(my_rectangle_1)
-print("--")
-my_rectangle_1.print_symbol = "&"
-print(my_rectangle_1)
-print("--")
 
-my_rectangle_2 = Rectangle(2, 1)
-print(my_rectangle_2)
-print("--")
-Rectangle.print_symbol = "C"
-print(my_rectangle_2)
-print("--")
+class Rectangle:
+    """Rectangle class defined by width and height.
 
-my_rectangle_3 = Rectangle(7, 3)
-print(my_rectangle_3)
+    Attributes:
+        number_of_instances: number of Rectangle instances,
+        increments with every instantitation,
+        decrements with every deletion
+        """
 
-print("--")
+        number_of_instances = 0
+        print_symbol = '#'
 
-my_rectangle_3.print_symbol = ["C", "is", "fun!"]
-print(my_rectangle_3)
+        def __init__(self, width=0, height=0):
+        """Initializes a Rectangle instance.
 
-print("--")
+        Args:
+            width: width of the rectangle
+            height: height of the rectangle
+            """
+            self.width = width
+            self.height = height
+            Rectangle.number_of_instances += 1
+
+            def __str__(self):
+            """Returns an informal and nicely printable string representation
+            of a Rectangle instance, filled with the '#' character."""
+            if self.__height == 0 or self.__width == 0:
+            return ''
+            rec_str = ''
+            for i in range(self.__height):
+            for j in range(self.__width):
+            rec_str += str(self.print_symbol)
+            rec_str += '\n'
+            return rec_str[:-1]
+
+            def __repr__(self):
+            """Return a string representation of a Rectangle instance
+            that is able to recreate a new instance by using eval()
+            """
+            return "Rectangle({}, {})".format(self.__width, self.__height)
+
+            def __del__(self):
+            """Deletes a Rectangle instance."""
+            print("Bye rectangle...")
+            Rectangle.number_of_instances -= 1
+
+            @property
+            def width(self):
+            """Retrieves the width of a Rectangle instance."""
+            return self.__width
+
+            @width.setter
+            def width(self, value):
+            """Sets the width of a Rectangle instance
+
+            Args:
+                value: value of the width, must be a positive integer
+                """
+                if not isinstance(value, int):
+                raise TypeError("width must be an integer")
+                if value < 0:
+                raise ValueError("width must be >= 0")
+                self.__width = value
+
+                @property
+                def height(self):
+                """Retrieves the height of a Rectangle instance."""
+                return self.__height
+
+                @height.setter
+                def height(self, value):
+                """Sets the height of a Rectangle instance
+
+                Args:
+                    value: value of the height, must be a positive integer
+                    """
+                    if not isinstance(value, int):
+                    raise TypeError("height must be an integer")
+                    if value < 0:
+                    raise ValueError("height must be >= 0")
+                    self.__height = value
+
+                    def area(self):
+                    """Calculates the area of a Rectangle instance
+
+                    Returns:
+                        Area of the the rectangle, given by height * width
+                        """
+                        return self.__width * self.__height
+
+                        def perimeter(self):
+                        """Calculates the perimeter of a Rectangle instance
+
+                        Returns:
+                            Perimeter of the rectangle, given by 2 * (height + width)
+                            """
+                            if self.__height == 0 or self.__width == 0:
+                            return 0
+                            return 2 * (self.__width + self.__height)
